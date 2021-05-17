ccb6fdc099557d351fd9079e3e26216089dd3988
RTaushanova <ral.taushanova@gmail.com>
2021-05-17 17:40:08 +0300

    Programming Basics with C#

---
+++ Programming Basics with C# June 2019/SquareStars/SquareStars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SquareStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string("*"; n));
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine("*" + new string(" "; n - 2) +"*");
+            }
+        }
+    }
+}
